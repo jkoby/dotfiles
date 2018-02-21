745e6e4beb18339a2564a5544b56e0958eb4fe19
Jay Koby <jkoby@splunk.com>
2018-02-21 09:14:15 -0500

    add splunk aliases

--- bashrc.d/alias.sh
+++ bashrc.d/alias.sh
@@ -6,7 +6,7 @@ alias _="sudo"
 
 ## Common utilities
 alias g="git"
-alias spl="splunk"
+alias sp="splunk"
 alias p2="python2"
 alias p3="python3"
 
@@ -17,8 +17,23 @@ alias move="mv"
 alias ren="mv"
 
 ## List declared aliases, functions, paths
-
 alias aliases="alias | sed 's/=.*//'"
 alias functions="declare -f | grep '^[a-z].* ()' | sed 's/{$//'"
 alias paths='echo -e ${PATH//:/\\n}'
 
+## Splunk aliases
+## (courtesy sjohnson)
+spec() {
+   less $SPLUNK_HOME/etc/system/README/${1}.conf.spec
+}
+ 
+alias spec=spec
+
+alias spapps="cd $SPLUNK_HOME/etc/apps"
+alias spdapps="cd $SPLUNK_HOME/etc/deployment-apps"
+alias spmapps="cd $SPLUNK_HOME/etc/master-apps"
+alias spslapps="cd $SPLUNK_HOME/etc/slave-apps"
+alias spshapps="cd $SPLUNK_HOME/etc/shcluster/apps"
+alias spesl="cd $SPLUNK_HOME/etc/system/local"
+alias spvls="cd $SPLUNK_HOME/var/log/splunk"
+alias spvlsd="less $SPLUNK_HOME/var/log/splunk/splunkd.log”
